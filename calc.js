8882c4f19cf310485bd3bb3b54fab1d690c0592e
MariaAndreaTrincanato <trincanato.mariaandrea1@gmail.com>
2020-04-07 17:05:03 +0200

    first commit

---
+++ calc.js
@@ -0,0 +1,26 @@
+calcScreen = document.getElementById("displayScreen");
+
+//numbers
+btn0 = document.getElementById("button0");
+btn1 = document.getElementById("button1");
+btn2 = document.getElementById("button2");
+btn3 = document.getElementById("button3");
+btn4 = document.getElementById("button4");
+btn5 = document.getElementById("button5");
+btn6 = document.getElementById("button6");
+btn7 = document.getElementById("button7");
+btn8 = document.getElementById("button8");
+btn9 = document.getElementById("button9");
+
+//operands
+btnPlus = document.getElementById('btn_plus');
+btnMinus = document.getElementById('btn_minus');
+btnProduct = document.getElementById('btn_product');
+btnDivision = document.getElementById('btn_division');
+
+//functions
+btnPercent = document.getElementById('btn_perc');
+btnSign = document.getElementById('btn_sign');
+btnErase = document.getElementById('btn_erase');
+btnComa = document.getElementById('btn_coma');
+btnRes = document.getElementById('btn_result');
